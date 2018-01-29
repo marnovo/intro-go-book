430446aa20df6f9d4369d36d2a3b4023f2b40ae6
Marcelo Novaes <mnovaes@gmail.com>
2018-01-29 18:01:46 -0200

    Add chapter 2
    Signed-off-by: Marcelo Novaes <mnovaes@gmail.com>

---
+++ ch2/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main()  {
+	// Strings
+	fmt.Println(len("Hello, World"))
+    fmt.Println("Hello, World"[1])
+	fmt.Println("Hello, " + "World")
+	
+	// Booleans
+	fmt.Println(true && true)
+    fmt.Println(true && false)
+    fmt.Println(true || true)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
